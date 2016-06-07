ffb178e5edc1474ef30fc48fab349324e7ba105c
amarparmar <amarsp666@gmail.com>
2016-06-06 22:03:46 -0400

    ..

---
+++ src/main/java/seleniumAuto/seleniumAuto/App.java
@@ -0,0 +1,13 @@
+package seleniumAuto.seleniumAuto;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
